9af6e96d5072da932f5238004d09ba475eb466b9
James Sinkler <jamessinkler713@gmail.com>
2020-10-03 19:08:54 -0700

    adds phone div, changes video to inside of phone div

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,15 +1,39 @@
 import React, {useState, useEffect, useContext} from 'react'; 
 import { Link } from 'gatsby'
-import Layout from '../components/layout'
+import ProjectLayout from '../components/ProjectLayout'
+import '../phoneStyle.css'
+import { useSpring } from 'react-spring'
  
 export default ()=> { 
+  const [show1, setShow1] = useState(false)
+  
   return (
-    <Layout>
-     <div> 
+    <ProjectLayout style={{maxWidth: '800px'}}>
        <h1>My Projects</h1>
-       <video style={{width:'375px', height: '669px'}} src='https://i.imgur.com/OLIbE7R.mp4' autoplay='true' /> 
-       <video style={{width:'375px', height: '669px'}} src='https://i.imgur.com/r3gJurD.mp4' autoplay='true' /> 
-     </div> 
-    </Layout>
+        <div className='projects'>
+         <div className='project project1'>
+           <div className='phone'>
+              <div className='phone--top'>
+                <div className='speaker'>
+                </div>
+              </div>
+              <div className='phone--content'>
+                <video style={{width:'100%', height: '100%'}} src='https://i.imgur.com/KUHv443.mp4' autoplay='true' /> 
+              </div>
+              <div className='phone--bottom'>
+                <div onClick={()=> setShow1(!show1)} className='circle'>
+                </div>
+              </div>
+           </div>
+           { show1?
+           <div className='project-info'>
+             <p>React Project, using an external weather API, parsing over 10,000 lines of weather data, filtered down to a human readable 5 day forecast.</p>
+           </div>
+           : ''
+           }
+         </div>
+         <video style={{width:'375px', height: '669px'}} src='https://i.imgur.com/OLIbE7R.mp4' autoplay='true' /> 
+        </div>
+    </ProjectLayout>
   ) 
 } 
