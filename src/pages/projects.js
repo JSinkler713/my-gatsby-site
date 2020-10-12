d877e7fc2c3373674337fba1ddb6cb3981893add
James Sinkler <jamessinkler713@gmail.com>
2020-10-12 13:03:24 -0700

    updates to project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,7 @@ import { useSpring } from 'react-spring'
  
 export default ()=> { 
   const [show1, setShow1] = useState(false)
+  const [show2, setShow2] = useState(false)
   
   return (
     <ProjectLayout style={{maxWidth: '800px'}}>
@@ -26,13 +27,49 @@ export default ()=> {
               </div>
            </div>
            { show1?
-           <div className='project-info'>
+           <div className='project-info-1'>
+             <h2>Weather-Report</h2>
              <p>React Project, using an external weather API, parsing over 10,000 lines of weather data, filtered down to a human readable 5 day forecast.</p>
+             <h3>Tech</h3>
+             <ul>
+               <li>React</li>
+               <li>OpenWeatherMap API</li>
+               <li>ES-6 Javascript</li>
+             </ul>
            </div>
            : ''
            }
          </div>
-         <video style={{width:'375px', height: '669px'}} src='https://i.imgur.com/OLIbE7R.mp4' autoplay='true' /> 
+         <div className='project project2'>
+           <div className='phone'>
+              <div className='phone--top'>
+                <div className='speaker'>
+                </div>
+              </div>
+              <div className='phone--content'>
+                <video style={{width:'100%', height: '100%'}} src='https://i.imgur.com/71bmeiy.mp4' autoplay='true' /> 
+              </div>
+              <div className='phone--bottom'>
+                <div onClick={()=> setShow2(!show2)} className='circle'>
+                </div>
+              </div>
+           </div>
+           { show2?
+           <div className='project-info-2'>
+             <h2>MovieReels</h2>
+             <p>A FullStack project, built with node, express, SQL on the backend. On the front-end using React. Created to help people remember and share the movies they love.</p>
+             <h3>Tech</h3>
+             <ul>
+               <li>React</li>
+               <li>OMDP API</li>
+               <li>ES-6 Javascript</li>
+               <li>SQL, using SQLite</li>
+               <li>Express, Node.js</li>
+             </ul>
+           </div>
+           : ''
+           }
+         </div>
         </div>
     </ProjectLayout>
   ) 
