aa0a13d163531cd8f2af72575e14e8e22512d147
James Sinkler <jamessinkler713@gmail.com>
2021-04-21 15:45:05 -0700

    wrap blogs in PostsWrapper

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import React from 'react';
 // this has same API as reach-router
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import PostsWrapper from '../components/PostsWrapper';
 import PostPreviewCard from '../components/post-preview-card'
 import usePosts from '../hooks/use-posts'
 
@@ -10,11 +11,12 @@ export default () => {
   const posts = usePosts()
 
   return(
-    <Layout>
-      <h1>Blog Posts</h1>
+      <Layout>
+      <PostsWrapper>
         {posts.map(post=> (
           <pre><PostPreviewCard key={post.slug} post={post}/></pre>
         ))}
-    </Layout>
+      </PostsWrapper>
+      </Layout>
   )
 }
