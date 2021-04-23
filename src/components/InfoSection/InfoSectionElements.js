0bf0c298449909e8ad65f52316b9ff627269a158
James Sinkler <jamessinkler713@gmail.com>
2021-04-22 17:17:21 -0700

    adds media query for image resize at smaller screens

--- src/components/InfoSection/InfoSectionElements.js
+++ src/components/InfoSection/InfoSectionElements.js
@@ -100,7 +100,11 @@ height: 100%;
 `
 
 export const Img = styled.img`
-width: 100%;
-height: 100%;
-margin: 0 0 10px 0;
+  width: 100%;
+  height: 100%;
+  margin: 0 0 10px 0;
+  @media (max-width: 768px) {
+    width: 60%;
+    height: 60%;
+  }
 `
