7e40cd9a4187ba7dfa33143f828bf87510b253c5
James Sinkler <jamessinkler713@gmail.com>
2021-04-23 17:59:59 -0700

    media query in place for hamburger

--- src/components/header.js
+++ src/components/header.js
@@ -61,10 +61,6 @@ const Header = ({toggleOpen}) => (
       align-items: center;
       padding: 0.5rem 2rem;
       z-index: 2;
-      @media screen and (max-width: 565px) {
-        /* going to need to show a hamburger menu */
-        display: none;
-      }
     `}
   >
     <NavLink fontWeight='bold' to='/'>Jsinkler Dev</NavLink>
