87d098fa70bae0c37e1d24b8d4d4f0837721a68a
James Sinkler <jamessinkler713@gmail.com>
2021-04-26 13:02:49 -0700

    take out header

--- src/components/getInTouch.js
+++ src/components/getInTouch.js
@@ -3,9 +3,6 @@ import styled from '@emotion/styled'
 import {  FaGithub, FaLinkedin } from 'react-icons/fa'
 import { GrMailOption, GrMail } from 'react-icons/gr' 
 const GetInTouchStyles = styled.div`
-  h2 {
-    color: white;
-  }
   svg {
     height: 36px;
     width: 36px;
@@ -14,24 +11,28 @@ const GetInTouchStyles = styled.div`
     color: white;
     list-style-type: none;
     margin: 0px;
+    width: 100%;
     padding: 0px;
     display: flex;
     flex-direction: row;
+    justify-content: space-around
   }
-  width: 300px;
-  margin: 10vw auto;
+  margin: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   li {
-    margin: 10px auto;
     text-decoration: none;
   }
   a, a:visited {
     color: white;
   }
+  a:hover {
+    color: #01BF71;
 `
 const GetInTouch = ()=> { 
   return ( 
     <GetInTouchStyles>
-      <h2>Get In Touch</h2>
       <ul> 
         <li><a href='https://www.linkedin.com/in/jsinkler-713/'><FaLinkedin /></a></li>
         <li><a href='https://github.com/JSinkler713'><FaGithub /></a></li>
