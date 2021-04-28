bbe0d0a6f9cbc3c8196039a55a004d0af2a044ad
James Sinkler <jamessinkler713@gmail.com>
2021-04-28 09:14:02 -0700

    structures MDXRenderer and MDXProvider

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,9 +3,21 @@ import Helmet from 'react-helmet'
 import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { MDXProvider } from '@mdx-js/react'
+import styled  from '@emotion/styled'
 import HomeLayout from '../components/homelayout';
 import ReadLink from '../components/readlink';
 import useIsOpen from '../hooks/use-isOpen';
+
+//make a styles h3
+const H3 = styled.h3`
+  margin-top: 1rem;
+  font-family: Poppins;
+  text-decoration: underline;
+`
+const components = {
+  h3: H3
+}
 
 // this picks up from context the slug we defined
 // we have the slug passed though a forEach in gatsby=node.
@@ -36,7 +48,9 @@ const PostTemplate = ({ data: { mdx: post } }) => {
       </Helmet>
     <h1>{post.frontmatter.title}</h1>
     <p css={css`font-size: 0.75rem`}>{post.frontmatter.author}</p>
-    <MDXRenderer>{post.body}</MDXRenderer>
+    <MDXProvider components={components}> 
+      <MDXRenderer>{post.body}</MDXRenderer>
+    </MDXProvider>
     <ReadLink to='/'>Back to Home</ReadLink>
   </HomeLayout>
   )
