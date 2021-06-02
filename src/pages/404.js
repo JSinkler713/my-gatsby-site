2f407be0799590df60d312a7784b7dd4e3e18189
James Sinkler <jamessinkler713@gmail.com>
2021-06-02 09:44:20 -0700

    404 w/ redirect and link to homepage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
-import { navigate } from 'gatsby';
+import { navigate, Link } from 'gatsby';
+import { StaticImage } from "gatsby-plugin-image"
 
 const Wrapper = styled.div`
-  margin-top: 30px;
+  position: absolute;
+  z-index: 1;
+  top: 50px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 100vw;
+  color: black;
 `;
 
 const NewComp = () => {
@@ -16,7 +21,7 @@ const NewComp = () => {
   useEffect(() => {
     let timer = setTimeout(() => {
       navigate('/');
-    }, 4000);
+    }, 5000);
     return () => clearTimeout(timer);
   }, []);
 
@@ -28,10 +33,23 @@ const NewComp = () => {
   }, [count]);
 
   return (
+    <>
+      <StaticImage
+        src="../project-assets/wave.jpg"
+        alt="a wave"
+        placeholder="blurred"
+        layout="fullWidth"
+        transformOptions={{grayscale: 'rgba(0, 0, 0, 0.2)'}}
+      />
+      Photo by <a href="https://unsplash.com/@matthardy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Matt Hardy</a> on <a href="https://unsplash.com/s/photos/waves?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
     <Wrapper>
-      <h1>Oop, this isn't one of my pages</h1>
-      <h2>Expect to be returned to the homepage in {count}</h2>
+      <h1>404</h1>
+      <h2>Oop, this isn't one of my pages</h2>
+      <h2>You will be returned to the <Link to='/'>homepage</Link>{(count <=3 && count > 0) ? (' in '+ count) : ''}</h2>
+
     </Wrapper>
+    </>
   );
 };
 export default NewComp;
