29c1149363db967c1060ee7957a0aee81fc6db3f
James Sinkler <jamessinkler713@gmail.com>
2021-06-03 10:03:11 -0700

    updated missing dependency

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import styled from '@emotion/styled';
 import { navigate, Link } from 'gatsby';
-import { StaticImage } from "gatsby-plugin-image"
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from '@emotion/styled';
 
 const Wrapper = styled.div`
   position: absolute;
@@ -39,16 +39,24 @@ const NewComp = () => {
         alt="a wave"
         placeholder="blurred"
         layout="fullWidth"
-        transformOptions={{grayscale: 'rgba(0, 0, 0, 0.2)'}}
+        transformOptions={{ grayscale: 'rgba(0, 0, 0, 0.2)' }}
       />
-      Photo by <a href="https://unsplash.com/@matthardy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Matt Hardy</a> on <a href="https://unsplash.com/s/photos/waves?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-
-    <Wrapper>
-      <h1>404</h1>
-      <h2>Oop, this isn't one of my pages</h2>
-      <h2>You will be returned to the <Link to='/'>homepage</Link>{(count <=3 && count > 0) ? (' in '+ count) : ''}</h2>
-
-    </Wrapper>
+      Photo by{' '}
+      <a href="https://unsplash.com/@matthardy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Matt Hardy
+      </a>{' '}
+      on{' '}
+      <a href="https://unsplash.com/s/photos/waves?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Unsplash
+      </a>
+      <Wrapper>
+        <h1>404</h1>
+        <h2>Oop, this isn't one of my pages</h2>
+        <h2>
+          You will be returned to the <Link to="/">homepage</Link>
+          {count <= 3 && count > 0 ? ' in ' + count : ''}
+        </h2>
+      </Wrapper>
     </>
   );
 };
