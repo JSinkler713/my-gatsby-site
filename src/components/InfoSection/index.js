415d3640cebdbafb4eaaec99dece0c95463f9e83
James Sinkler <jamessinkler713@gmail.com>
2021-06-03 11:15:48 -0700

    remove unneccesary dependencies, prettier

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,32 +1,71 @@
-import React, {useState, useEffect, useContext} from 'react'; 
-import { InfoContainer, InfoWrapper, Row, Column, TextWrapper, TopLine, Heading, Subtitle, Button, BtnWrap, ImgWrap, Img} from './InfoSectionElements'
+import {
+  InfoContainer,
+  InfoWrapper,
+  Row,
+  Column,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  Button,
+  BtnWrap,
+  ImgWrap,
+  Img,
+} from './InfoSectionElements';
 
-
-const InfoSection = ({vh, button, lightBg, mTop, mkSmall, id, imgStart, lightText, topLine, headline, description, img, alt, primary, buttonLabel, dark})=> {
+const InfoSection = ({
+  vh,
+  button,
+  lightBg,
+  mTop,
+  mkSmall,
+  id,
+  imgStart,
+  lightText,
+  topLine,
+  headline,
+  description,
+  img,
+  alt,
+  primary,
+  buttonLabel,
+  dark,
+}) => {
   return (
     <>
       <InfoContainer lightBg={lightBg} id={id}>
         <InfoWrapper vh={vh} mTop={mTop}>
           <Row imgStart={imgStart}>
-            <Column colNum={'1'} >
+            <Column colNum={'1'}>
               <TextWrapper>
                 <TopLine>{topLine}</TopLine>
                 <Heading lightText={lightText}>{headline}</Heading>
                 <Subtitle lightText={lightText}>{description}</Subtitle>
                 <BtnWrap>
-                  {button ? <Button primary={primary} dark={dark} lightText={lightText} to={button}>{buttonLabel}</Button> : ''}
+                  {button ? (
+                    <Button
+                      primary={primary}
+                      dark={dark}
+                      lightText={lightText}
+                      to={button}
+                    >
+                      {buttonLabel}
+                    </Button>
+                  ) : (
+                    ''
+                  )}
                 </BtnWrap>
               </TextWrapper>
             </Column>
-            <Column colNum={'2'} >
+            <Column colNum={'2'}>
               <ImgWrap>
-                <Img src={img} mkSmall={mkSmall} alt={alt}/>
+                <Img src={img} mkSmall={mkSmall} alt={alt} />
               </ImgWrap>
             </Column>
           </Row>
         </InfoWrapper>
-      </InfoContainer >
+      </InfoContainer>
     </>
-  )
-}
+  );
+};
 export default InfoSection;
