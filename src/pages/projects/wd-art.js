738f5ac3ca33f7d66c338a93960235f670a2dd28
James Sinkler <jamessinkler713@gmail.com>
2021-06-22 14:08:08 -0700

    mostly set up, need technologies

--- src/pages/projects/wd-art.js
+++ src/pages/projects/wd-art.js
@@ -8,6 +8,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Title, } from '../../components/portfolioElements';
+import { TechName, TechnologyHolder, ImageContainer, SubHeader, Container, Content} from '../../components/portfolioPageElements'
 
 export default function() {
   //const [show1, setShow1] = useState(true);
@@ -27,6 +28,47 @@ export default function() {
         layout="fullWidth"
       />
       <Title>WDArt</Title>
+      <Container>
+        <Content>
+          <SubHeader>Description</SubHeader>
+          <p>A digital storefront for amazing photography. Stripe implementation for secure payment checkout, with Gatsby powering a fast static site, and React implemented for actions such as the shopping cart, and buy functionality.</p>
+          <ImageContainer>
+            {/* TODO: GET BETTER SCREENSHOT */}
+               <StaticImage
+                src='../../portfolio-assets/wdart-second-pic.png'
+                alt="A phone displaying the weather"
+                placeholder="blurred"
+                layout="fullWidth"
+              />
+          </ImageContainer>
+          <SubHeader center={true}>Technologies</SubHeader>
+          <TechnologyHolder>
+               <StaticImage
+                src='../../portfolio-assets/wdart-third-pic.png'
+                alt="A phone displaying the weather"
+                placeholder="blurred"
+                layout="fixed"
+                width={64}
+                height={64}
+              />
+            <TechName>React</TechName>
+            
+          </TechnologyHolder>
+          <SubHeader>Summary</SubHeader>
+          <p>A responsive, fast, easy to navigate storefront to buy digital assets. The stripe implementation combined with React’s useContext hook for cart management, makes this this a full featured storefront.</p>
+          <ImageContainer>
+               <StaticImage
+                src='../../portfolio-assets/wdart-third-pic.png'
+                alt="A phone displaying the weather"
+                placeholder="blurred"
+                layout="fullWidth"
+              />
+          </ImageContainer>
+          <SubHeader>Want More?</SubHeader>
+          <p>Feel free to browse the code, or check out the live site with the following links.</p>
+          <p>Code at <a href='https://github.com/JSinkler713/wall-deco'>GitHub</a> / <a href='https://wall-art-deco.netlify.app/' >Live Site </a>.</p>
+        </Content>
+      </Container>
     </HomeLayout>
   )
 }
