06f272c67736c17d9ed7a5ca93e608edc496686b
James Sinkler <jamessinkler713@gmail.com>
2021-06-21 18:32:49 -0700

    check routing system in subdirectory, all good

--- src/pages/tomato/index.js
+++ src/pages/tomato/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'gatsby';
 
 const NewComp = () => {
   return(
     <div>
       Hello :) 😁.
+      <Link to='/tomato/what'>To the what page</Link>
     </div>
   )
 };
---
+++ src/pages/tomato/what.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect, useContext} from 'react'; 
+ 
+const What = ()=> { 
+  return ( 
+     <div> 
+       <h1>What a nice routing system</h1>
+       <p>😌</p>
+       
+  </div>  ) 
+} 
+export default What;
