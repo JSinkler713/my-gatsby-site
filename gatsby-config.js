08fb7a3a7564537b82f60567f31f0746948ba10c
James Sinkler <jamessinkler713@gmail.com>
2021-06-22 12:49:26 -0700

    CACHE not being cleared auto anymore: use `gatsby clean` if needed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  flags: {
+    PRESERVE_FILE_DOWNLOAD_CACHE: true,
+    PRESERVE_WEBPACK_CACHE: true
+  },
   siteMetadata: {
     title: 'James Sinkler Developer',
     description: 'A site powered by Gatsby and React, built by James Sinkler'
