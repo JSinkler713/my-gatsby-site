b00cc20a60b0f1b9fc8a66574b0bc07b8ea670ac
James Sinkler <jamessinkler713@gmail.com>
2021-06-22 20:36:52 -0700

    finish up wd-art working version

--- src/pages/projects/wd-art.js
+++ src/pages/projects/wd-art.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import HomeLayout from '../../components/homelayout';
 import { Link } from 'gatsby';
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 // import '../phoneStyle.css'
 import { useSpring } from 'react-spring';
 import useIsOpen from '../../hooks/use-isOpen';
@@ -8,12 +9,15 @@ import { StaticImage } from 'gatsby-plugin-image';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Title, } from '../../components/portfolioElements';
-import { TechName, TechnologyHolder, ImageContainer, SubHeader, Container, Content} from '../../components/portfolioPageElements'
+import { IconTitle, TechWrapper, IconWrapper, TechnologyHolder, ImageContainer, SubHeader, Container, Content} from '../../components/portfolioPageElements'
+import IconContainer from '../../components/IconContainer'
+
 
 export default function() {
   //const [show1, setShow1] = useState(true);
   //const [show2, setShow2] = useState(true);
   const [isOpen, toggleOpen] = useIsOpen();
+
 
   return (
     <HomeLayout
@@ -42,18 +46,90 @@ export default function() {
               />
           </ImageContainer>
           <SubHeader center={true}>Technologies</SubHeader>
-          <TechnologyHolder>
-               <StaticImage
-                src='../../portfolio-assets/wdart-third-pic.png'
-                alt="A phone displaying the weather"
-                placeholder="blurred"
-                layout="fixed"
-                width={64}
-                height={64}
-              />
-            <TechName>React</TechName>
-            
-          </TechnologyHolder>
+          <TechWrapper>
+            <TechnologyHolder>
+              <IconWrapper>
+                <StaticImage
+                  src='../../icons/Sanity_CMS.png'
+                  alt="Sanity_CMS"
+                  placeholder="blurred"
+                  layout="fixed"
+                  height={64}
+                />
+                <IconTitle>Sanity CMS</IconTitle>
+              </IconWrapper>
+              <IconWrapper>
+                <StaticImage
+                  src='../../icons/Stripe.png'
+                  alt="Stripe"
+                  placeholder="blurred"
+                  layout="fixed"
+                  height={64}
+                />
+                <IconTitle>Stripe</IconTitle>
+              </IconWrapper>
+              <IconWrapper>
+                <StaticImage
+                  src='../../icons/React.png'
+                  alt="React"
+                  placeholder="blurred"
+                  layout="fixed"
+                  height={64}
+                />
+                <IconTitle>React</IconTitle>
+              </IconWrapper>
+              <IconWrapper>
+                <StaticImage
+                  src='../../icons/Styled_Components.png'
+                  alt="Styled_Components"
+                  placeholder="blurred"
+                  layout="fixed"
+                  height={64}
+                />
+                <IconTitle>Styled Components</IconTitle>
+              </IconWrapper>
+              <IconWrapper>
+                <StaticImage
+                  src='../../icons/React_Spring.png'
+                  alt="React_Spring"
+                  placeholder="blurred"
+                  layout="fixed"
+                  height={64}
+                />
+                <IconTitle>React Spring</IconTitle>
+              </IconWrapper>
+              <IconWrapper>
+                <StaticImage
+                  src='../../icons/Gatsby.png'
+                  alt="Gatsby"
+                  placeholder="blurred"
+                  layout="fixed"
+                  height={64}
+                />
+                <IconTitle>Gatsby</IconTitle>
+              </IconWrapper>
+              <IconWrapper>
+                <StaticImage
+                  src='../../icons/Netlify.png'
+                  alt="Netlify"
+                  placeholder="blurred"
+                  layout="fixed"
+                  height={64}
+                />
+                <IconTitle>Netlify</IconTitle>
+              </IconWrapper>
+              <IconWrapper>
+                <StaticImage
+                  src='../../icons/GitHub.png'
+                  alt="GitHub"
+                  placeholder="blurred"
+                  layout="fixed"
+                  height={64}
+                />
+                <IconTitle>GitHub</IconTitle>
+              </IconWrapper>
+            </TechnologyHolder>
+          </TechWrapper>
           <SubHeader>Summary</SubHeader>
           <p>A responsive, fast, easy to navigate storefront to buy digital assets. The stripe implementation combined with React’s useContext hook for cart management, makes this this a full featured storefront.</p>
           <ImageContainer>
